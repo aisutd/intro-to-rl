ea822dc1cda4938e1cd5bae81a049c9ce63edd1c
aephus <aaron.chang384@gmail.com>
2021-11-09 17:29:11 -0600

    added code comments

--- PPO_agent.py
+++ PPO_agent.py
@@ -7,6 +7,7 @@ from gym import wrappers, logger
 
 env = gym.make("CartPole-v0")
 
+#   code to create videos of the episodes
 outdir = "./recordings"
 env = wrappers.Monitor(env, directory=outdir, force=True, uid="", write_upon_reset=False)
 
@@ -14,11 +15,14 @@ def run_episode(params):
     total_reward = 0
     observation = env.reset()
     while True:
+        #   apply policy to the observation and return an action
+        #   the @ symbol denotes matrix multiplication
         if param @ observation < 0:
             action = 0
         else:
             action = 1
         
+        #   this collects all the information given in each step of the environment
         observation, reward, done, info = env.step(action)
         total_reward += reward
         env.render()
@@ -33,11 +37,14 @@ done = False
 max_reward = 0
 
 for i in range(episode_count):
+    #   generate a random policy
     param = np.random.rand(4)
     total_reward = run_episode(param)
-
+    
+    #   track the current max reward
     max_reward = max(max_reward, total_reward)
 
+    #   if the max reward is 200 (the highest it can be) the policy is the best policy
     if max_reward >= 200:
         print("Best Policy: ", param)
     
--- random_agent.py
+++ random_agent.py
@@ -8,6 +8,7 @@ from gym import wrappers, logger
 
 env = gym.make("CartPole-v0")
 
+#   code to create videos of the episodes
 outdir = "./recordings"
 env = wrappers.Monitor(env, directory=outdir, force=True, uid="", write_upon_reset=False)
 
@@ -15,8 +16,11 @@ def run_episode():
     total_reward = 0
     observation = env.reset()
     while True:
+        #   randomly chooses 0 or 1
+        #   0 move the cart left, 1 moves the cart right
         action = randrange(2)
         
+        #   this collects all the information given in each step of the environment
         observation, reward, done, info = env.step(action)
         total_reward += reward
 
@@ -33,8 +37,10 @@ max_reward = 0
 for i in range(episode_count):
     total_reward = run_episode()
 
+    #   track the current max reward
     max_reward = max(max_reward, total_reward)
 
+#   the reward has a statistically very very very high chance to never reach 200 as it randomly moves the cart around
 print(max_reward)
 
 env.close()
